d0da6e14f8aee5a1006de223ec16aed966d5337f
Max Kachinkin <makzimi@gmail.com>
2023-06-01 08:55:38 +0300

    Add sp to DimensionsExt

--- app/src/main/java/ru/otus/canvas/utils/DimensionsExt.kt
+++ app/src/main/java/ru/otus/canvas/utils/DimensionsExt.kt
@@ -5,3 +5,5 @@ import android.content.res.Resources
 val Int.dp: Float
     get() = (this * Resources.getSystem().displayMetrics.density)
 
+val Int.sp: Float
+    get() = (this * Resources.getSystem().displayMetrics.scaledDensity)
